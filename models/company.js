a729b42665430c7cd216f629628c335d288f75ac
Nick Brown <n.brown.professional@gmail.com>
2018-05-07 20:12:47 -0600

    adds changes to readme, backend file structure, and basic html form

---
+++ models/company.js
@@ -0,0 +1,58 @@
+module.exports = function(sequelize, DataTypes) {
+  var Company = sequelize.define("Company", {
+    company_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [75]
+      }
+    },
+    position_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [75]
+      }
+    },
+    date_added: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [75]
+      }
+    },
+    website_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [2000]
+      }
+    },
+    hiring_manager_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [160]
+      }
+    },
+    hiring_manager_phone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [75]
+      }
+    },
+    hiring_manager_email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [75]
+      }
+    },
+    company_notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      len: [65,535]
+    }
+  });
+};
---
+++ schema.sql
@@ -0,0 +1,4 @@
+-- Drops the the_hunbt_db if it exists currently --
+DROP DATABASE IF EXISTS the_hunbt_db;
+-- Creates the "the_hunbt_db" database --
+CREATE DATABASE the_hunbt_db;
