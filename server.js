67faeaf11147d5446b3366e117bbffdcfc1d0936
Nick Brown <n.brown.professional@gmail.com>
2018-05-15 10:46:17 -0600

    -Adds changes to style, logic, apis, and handlebars --minor style changes to the background and box shadow --changes logic to allow ajax calls to update and delete for each company card --changes api routes to contact database to put and delete data --major changes to handlebars partials

--- server.js
+++ server.js
@@ -27,7 +27,11 @@ app.use(bodyParser.json());
 
 
 // Set Handlebars.
+var hbs = require('handlebars');
 
+hbs.registerHelper('ifEquals', function (arg1, arg2, options) {
+  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
