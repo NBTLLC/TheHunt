0228a219bd10335ec74395524b946f9b1da1fbdc
Morinventiv <brandonmorin@gmail.com>
2018-08-21 14:05:53 -0600

    removed about section

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var Sequelize = require('sequelize'), connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = new Sequelize(process.env.JAWSDB_URL);
+	connection = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  connection = new Sequelize('the_hunt_db', 'root', 'runthisCODE', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: '8020'
-  })
+	connection = new Sequelize('the_hunt_db', 'root', 'Password', {
+		host: 'localhost',
+		dialect: 'mysql',
+		port: '8020'
+	})
 };
 
 module.exports = connection;
